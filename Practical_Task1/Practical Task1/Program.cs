d9554cb610ab66736929bc26c1c496bf6298865a
Zakaria <elbakidze.zaqaria@gmail.com>
2022-12-22 22:07:58 +0400

    Development and build tools task

--- Practical_Task1/Practical Task1/Program.cs
+++ Practical_Task1/Practical Task1/Program.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 
-
 public class Task1
 {
-    public static void Read(string input, int len)
+    public static void Read(string input)
     {
-
-        char characters;
         char[] charArray = input.ToCharArray();
-        string s = new string(charArray);
-        //return s;
+        string charToString = new string(charArray);
         int count = 0;
-        for(int i = 1; i < s.Length; i++)
+
+        for (int i = 1; i < charToString.Length; i++)
         {
             count += 1;
-            if (!s[i - 1].Equals(charArray[i]))
-                Console.WriteLine($"{s[i]} {count + 1}");
+            if (!charToString[i - 1].Equals(charArray[i]))
+                Console.WriteLine($"{charToString[i]} {count + 1}");
             else
             {
                 Console.WriteLine("~");
-            } 
-            
+            }
         }
-        
     }
+
     public static void Main()
     {
         Console.WriteLine("repeated values will be replaced by \"~\"");
-        String str = Console.ReadLine();
-        int n = str.Length;
+        String inputString = Console.ReadLine();
         Console.WriteLine("\ninput results: ");
-        Console.WriteLine($"{str[0]} 1");
-        Read(str, n);
+        Console.WriteLine($"{inputString[0]} 1");
+        Read(inputString);
     }
 }

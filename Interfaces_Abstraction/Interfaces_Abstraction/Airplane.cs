4f9e530cd66f948dc1adc596f210861383ddadbd
Zakaria <elbakidze.zaqaria@gmail.com>
2023-03-30 23:13:13 +0400

    Interfaces and abstract classes

---
+++ Interfaces_Abstraction/Interfaces_Abstraction/Airplane.cs
@@ -0,0 +1,79 @@
+﻿namespace Interfaces.Abstraction;
+
+internal class Airplane : IFlyable
+{
+    public Coords currentPosition = new Coords(115, 145, 185);
+    //Constants storing values that should not be changed;
+    public const  double initialSpeed = 200; 
+    public const  double maxSpeed = 1040;    
+    public const  double maxDistance = 5600.52;
+    //storing random values in variables;
+    public static double flightTime = RandomTime(); 
+    public static double variableDistance = VariableDistance();
+
+    //Randomly generating flight time
+    public static int RandomTime() 
+    {
+        var rnd = new Random();
+        return rnd.Next(0, 6);
+    }
+
+    //Randomly generating variable distance
+    public static int VariableDistance() 
+    {
+        var rnd = new Random();
+        return rnd.Next(100, 933);
+    }
+
+    //calculating speed based on the distance travelled
+    public double AirPlaneSpeed() 
+    {
+        //Calculating distance travelled by the plane based on random values
+        double randomDistance = flightTime * variableDistance;
+        double currentSpeed = 0;
+
+        //iterating over the total distance value but then limiting the speed
+        for (double i = 0; i <= randomDistance; i+=10)  
+        {
+            //Limiting speed to 1040; +10km/h every 10km;
+            if (currentSpeed < maxSpeed & flightTime != 0) 
+            {
+                currentSpeed = initialSpeed + i; 
+            }
+        }
+
+        //throwing if plane speed goes below minimum speed
+        if (currentSpeed < initialSpeed & flightTime != 0)  
+        {
+            throw new Exception("Flight speed cannot go below 200km/h ");
+        }
+
+        //throwing if plane speed goes above maximum speed
+        if (currentSpeed > maxSpeed)  
+        {
+            throw new Exception("This plane cannot go beyond 1046km/h ");
+        }
+
+        return currentSpeed;
+    }
+
+    //This method and its memebers is not involved elsewhere, it just returns total distance travelled
+    public double FlyTo()  
+    {
+        //calculating total distance covered by the plane and throws exception if it goes beyond max distance
+        double distance = flightTime * variableDistance;
+        if (distance > maxDistance)                
+        {
+            throw new Exception("Airplanes cannot fly over 5600km ");
+        }
+
+        return distance;
+    }
+
+    //This method and its memebers is not involved elsewhere, it just returns total flight time
+    public double GetFlyTime() 
+    {
+        //returning flight time
+        return flightTime; 
+    }
+}
---
+++ Interfaces_Abstraction/Interfaces_Abstraction/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Interfaces.Abstraction;
+
+interface IFlyable
+{
+    double FlyTo();
+    double GetFlyTime();
+}
+
+public struct Coords
+{
+    public Coords(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public int Z { get; private set; }
+
+    public override string ToString()
+    {
+        return $"X:{X} Y:{Y} Z:{Z}";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Coords defaultCoords = new Coords();
+        Console.WriteLine("Default coords: " + defaultCoords);
+
+        Bird bird = new Bird();
+        Console.WriteLine("\nBird coords: " + bird.currentPosition);
+        Console.WriteLine("Bird traveled distance: " + bird.FlyTo() + "km");
+
+        Drone drone = new Drone();
+        Console.WriteLine("\nDrone coords: " + drone.currentPosition);
+        Console.WriteLine("Drone flight time: " + drone.GetFlyTime() + "min");
+        Console.WriteLine("Drone hover time: " + drone.HoverTime() + "min");
+        Console.WriteLine("Drone speed: " + Drone.flySpeed + "km/h");
+        Console.WriteLine("Distance covered by the drone: " + drone.FlyTo() + "km");
+
+        Airplane airplane = new Airplane();
+        Console.WriteLine("\nAirplane coords: " + airplane.currentPosition);
+        Console.WriteLine("Airplane flight time: " + airplane.GetFlyTime() + "hours");
+        Console.WriteLine("Airplane Speed: " + airplane.AirPlaneSpeed() + "km/h");
+        Console.WriteLine("Airplane flight distance: " + airplane.FlyTo() + "km");
+    }
+}

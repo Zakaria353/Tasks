4cad4402496979e07bca876af46cc3028171c96e
Zakaria <elbakidze.zaqaria@gmail.com>
2023-05-17 17:03:37 +0400

    OOP practical task

---
+++ Carpark/Carpark/Chassis.cs
@@ -0,0 +1,17 @@
+﻿namespace Carpark;
+
+public class Chassis
+{
+    public string? WheelsNumber { get; set; }
+    public decimal Number { get; set; }
+    public string? PermissibleLoad { get; set; }
+
+    public Chassis(string wheelsnumber, decimal number, string permissibleLoad)
+    {
+        WheelsNumber = wheelsnumber;
+        Number = number;
+        PermissibleLoad = permissibleLoad;
+    }
+
+    public override string ToString() => $"WheelsNumber:{WheelsNumber}|| Number:{Number}|| Permissibleload:{PermissibleLoad}";
+}
---
+++ Carpark/Carpark/Transmission.cs
@@ -0,0 +1,17 @@
+﻿namespace Carpark;
+
+public class Transmission
+{
+    public string? TransmissionType { get; set; }
+    public int NumberOfGears { get; set; }
+    public string? Manufacturer { get; set; }
+
+    public Transmission(string? transmissionType, int numberOfGears, string? manufacturer)
+    {
+        TransmissionType = transmissionType;
+        NumberOfGears = numberOfGears;
+        Manufacturer = manufacturer;
+    }
+
+    public override string ToString() => $"Type:{TransmissionType}|| NumberOfGears:{NumberOfGears}|| Manufacturer:{Manufacturer}";
+}
